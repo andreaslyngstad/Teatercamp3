1585c65e5420f264ff91ba61e201157a8ae20f33
Andreas Lyngstad <andreaslyngstad@gmail.com>
2011-09-14 23:13:24 +0200

    fix users

--- db/migrate/20110914194407_fix_users.rb
+++ db/migrate/20110914194407_fix_users.rb
@@ -1,7 +1,7 @@
 class FixUsers < ActiveRecord::Migration
   def self.up
-      add_column :users, :password_hash, :reminder_date, :string  
-      add_column :users, :password_salt, :reminder_date, :string
+      add_column :users, :password_hash, :string  
+      add_column :users, :password_salt, :string
       remove_column :users, :remember_token
       remove_column :users, :crypted_password
       remove_column :users, :remember_token_expires_at
