670b4b1ef77a8e230b5ac3b8b750629db9636ccf
Andreas Lyngstad <andreaslyngstad@gmail.com>
2011-09-14 14:37:06 +0200

    fix deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,9 +23,22 @@ namespace :deploy do
   desc "Symlink shared configs and folders on each release."
   task :symlink_shared do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-  end
-  
-  
+  end 
 end
+namespace :bundler do
+  desc "make the bundle"
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+ 
+after 'deploy:update_code', 'bundler:bundle_new_release'
 after 'deploy:update_code', 'deploy:symlink_shared'
 after "deploy", "deploy:cleanup"
