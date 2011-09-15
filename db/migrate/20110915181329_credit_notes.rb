14fb3508a8fcd7e0ea2ff1754d86d50d4a8aeb04
Andreas Lyngstad <andreaslyngstad@gmail.com>
2011-09-15 20:15:02 +0200

    fix migrations

--- db/migrate/20110915181329_credit_notes.rb
+++ db/migrate/20110915181329_credit_notes.rb
@@ -1,13 +1,14 @@
-class CreateCreditNotes < ActiveRecord::Migration
-   def self.up
+class CreditNotes < ActiveRecord::Migration
+  def self.up
     create_table :credit_notes do |t|
       t.integer :invoice_id
       t.float :total
 
       t.timestamps
-    end
   end
+
   def self.down
     drop_table :credit_notes
+  end
   end
 end
