9f5b15656ff0f27f356d5a831cad7dcf9164d3a6
Andreas Lyngstad <andreaslyngstad@gmail.com>
2016-01-25 08:33:28 -0500

    bcrypt

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ set :bundle_path, '/var/www/vhosts/teatercamp.no/httpdocs/teatercamp.no/gems'
 
 # Default value for :linked_files is []
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, %w{'config/database.yml', 'config/email.yml'}
+set :linked_files, %w{config/database.yml config/email.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 # Default value for linked_dirs is []
 # set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
