ae933251e4799a6cb289a0d6cb482f23bff89810
Andreas Lyngstad <andreaslyngstad@gmail.com>
2018-11-22 14:51:40 +0100

    update_capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,42 +1,43 @@
-# require 'capistrano/bundler'
-
-# require 'capistrano/rails/migrations'
-# config valid only for current version of Capistrano
-lock '3.11.0'
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
 
 set :application, 'teatercamp'
 set :repo_url, 'git@github.com:andreaslyngstad/Teatercamp3.git'
-
-
-# Default branch is :master
-set :branch, 'master'
-
-# Default deploy_to directory is /var/www/my_app_name
-
 set :deploy_to, "/var/www/vhosts/teatercamp.no/httpdocs/teatercamp.no"
-
-# Default value for :scm is :git
-
 set :bundle_path, '/var/www/vhosts/teatercamp.no/httpdocs/teatercamp.no/gems'
 
-# Default value for :format is :pretty
-# set :format, :pretty
+set :linked_files, %w{config/database.yml config/email.yml}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default value for :log_level is :debug
-# set :log_level, :debug
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-set :linked_files, %w{config/database.yml config/email.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+# append :linked_files, "config/database.yml"
+
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
