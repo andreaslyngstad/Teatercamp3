b9896dfe8a69b868618b033ce6bad07c4ad5cf63
Andreas Lyngstad <andreaslyngstad@gmail.com>
2018-11-24 00:04:15 +0100

    update_fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,7 +5,7 @@ Teatercamp3::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-  config.serve_static_assets = false
+  config.serve_static_assets = true
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
