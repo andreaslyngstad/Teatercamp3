d9eca01d118463abaa5be88bedb3b9215ef52867
Andreas Lyngstad <andreaslyngstad@gmail.com>
2018-11-23 22:50:52 +0100

    update_fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,10 @@ Teatercamp3::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-
+  config.serve_static_assets = false
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.assets.precompile += %w[admin.css, forms-min.css]
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
