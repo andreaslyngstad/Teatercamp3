df1370dbc935745cb832eee1bf4b20bc2fda28f4
Andreas Lyngstad <andreaslyngstad@gmail.com>
2018-11-23 23:27:43 +0100

    update_fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -9,7 +9,7 @@ Teatercamp3::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.assets.precompile += %w[admin.css, forms-min.css]
+  config.assets.precompile += %w[admin.css.scss, forms-min.css.scss]
   # Specifies the header that your server uses for sending files
   config.action_dispatch.x_sendfile_header = "X-Sendfile"
 
