7d8979e590faf480f7e1b93e27928ef585cc1c3c
andreaslyngstad <andreaslyngstad@gmail.com>
2022-02-04 12:55:24 +0100

    deploy fixing

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,6 +8,24 @@ set :bundle_path, '/var/www/vhosts/teatercamp.no/httpdocs/gems'
 
 set :linked_files, %w{config/database.yml config/email.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+
+# namespace :deploy do
+#   namespace :assets do
+#       task :precompile, :roles => :web do
+#         from = source.next_revision(current_revision)
+#         if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+#           run_locally("rake assets:clean && rake assets:precompile")
+#           run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+#           top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+#           run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+#           run_locally "rm public/assets.tar.bz2"
+#           run_locally("rake assets:clean")
+#         else
+#           logger.info "Skipping asset precompilation because there were no asset changes"
+#         end
+#       end
+#     end
+#   end
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
